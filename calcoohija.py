34e4a5914c1b1d279c24e0d00e8f6bdb2238c254
raquel paunero herrero <rpaunero@zeta40.aulas.gsyc.es>
2015-09-28 17:11:30 +0200

    calcoohija.py import calcoo

--- calcoohija.py
+++ calcoohija.py
@@ -2,15 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-class Calculadora():
-	def plus(self, op1, op2):
-		return op1 + op2
-
-	def minus(self, op1, op2):
-		return op1 - op2
+import calcoo
 	
-class CalculadoraHija(Calculadora):
+class CalculadoraHija(calcoo.Calculadora):
 	def mult(self, op1, op2):
 		return op1 * op2
 
