f4b7aeb5ffb96d20c53b3c85f4bd173cac4a1fbf
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-01 11:57:14 +0200

    Mejora

--- calcplus.py
+++ calcplus.py
@@ -9,33 +9,33 @@ fich = open(fichero,'r')
 lineas = fich.readlines()
 calc = calcoohija.CalculadoraHija()
 
-resultado_suma = 0
-resultado_resta = 0
-resultado_division = 0
-resultado_multiplicacion = 0
+#resultado_suma = 0
+#resultado_resta = 0
+#resultado_division = 0
+#resultado_multiplicacion = 0
 for linea in lineas:
 	#print(linea)
 	linea = linea[:-1]
 	dato = linea.split(',')
 	if dato[0] == "suma":
 		for elemento in dato[1:]:
-			resultado_suma = calc.plus(int(resultado_suma), int(elemento))
+			resultado = calc.plus(int(resultado), int(elemento))
 		print('suma= ',resultado_suma)
 	if dato[0] == "resta":
-		resultado_resta = calc.minus(int(dato[1]), int(dato[2]))
+		resultado = calc.minus(int(dato[1]), int(dato[2]))
 		for elemento in dato[3:]:
-			resultado_resta = calc.minus(int(resultado_resta), int(elemento))
+			resultado = calc.minus(int(resultado), int(elemento))
 		print('resta=', resultado_resta)
 	if dato[0] == "divide":
-		resultado_division = calc.div(int(dato[1]), int(dato[2]))
+		resultado = calc.div(int(dato[1]), int(dato[2]))
 		for elemento in dato[3:]:
 			if elemento == 0:
 				sys.exit("Division by zero is not allowed")
 			else:
-				resultado_division = calc.div(int(resultado_division), int(elemento))
-		print('division=' ,resultado_division)
+				resultado = calc.div(int(resultado), int(elemento))
+		print('division=' ,resultado)
 	if dato[0] == "multiplica":
-		resultado_multiplicacion = calc.mult(int(dato[1]), int(dato[2]))
+		resultado = calc.mult(int(dato[1]), int(dato[2]))
 		for elemento in dato[3:]:
-			resultado_multiplicacion = calc.mult(int(resultado_multiplicacion), int(elemento))
-		print('multiplicacion=', resultado_multiplicacion)
+			resultado = calc.mult(int(resultado), int(elemento))
+		print('multiplicacion=', resultado)
