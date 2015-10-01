8be2316bed05843ce6f5275d28bd9e952f18571f
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-01 11:58:43 +0200

    Mejora

--- calcplus.py
+++ calcplus.py
@@ -13,6 +13,7 @@ calc = calcoohija.CalculadoraHija()
 #resultado_resta = 0
 #resultado_division = 0
 #resultado_multiplicacion = 0
+resultado = 0
 for linea in lineas:
 	#print(linea)
 	linea = linea[:-1]
@@ -20,12 +21,12 @@ for linea in lineas:
 	if dato[0] == "suma":
 		for elemento in dato[1:]:
 			resultado = calc.plus(int(resultado), int(elemento))
-		print('suma= ',resultado_suma)
+		print('suma= ',resultado)
 	if dato[0] == "resta":
 		resultado = calc.minus(int(dato[1]), int(dato[2]))
 		for elemento in dato[3:]:
 			resultado = calc.minus(int(resultado), int(elemento))
-		print('resta=', resultado_resta)
+		print('resta=', resultado)
 	if dato[0] == "divide":
 		resultado = calc.div(int(dato[1]), int(dato[2]))
 		for elemento in dato[3:]:
