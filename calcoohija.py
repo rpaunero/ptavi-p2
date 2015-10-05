886cc97eb3156470047b732743b997b6d1022b3c
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-05 10:38:07 +0200

    Pep8

--- calcoohija.py
+++ calcoohija.py
@@ -3,35 +3,33 @@
 
 import sys
 import calcoo
-	
+
+
 class CalculadoraHija(calcoo.Calculadora):
-	def mult(self, op1, op2):
-		return op1 * op2
+    def mult(self, op1, op2):
+        return op1 * op2
 
-	def div(self, op1, op2):
-		return op1 / op2
-
-	
+    def div(self, op1, op2):
+        return op1 / op2
 
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	calc = CalculadoraHija()
+    calc = CalculadoraHija()
 
-
-	if sys.argv[2] == "suma":
-		result = calc.plus(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = calc.minus(operando1, operando2)
-	elif sys.argv[2] == "multiplica":
-		result = calc.mult(operando1, operando2)
-	elif sys.argv[2] == "divide":
-			if operando2 == 0:
-				sys.exit("Division by zero is not allowed")
-			else:
-				result = calc.div(operando1, operando2)
-	print(result)
+    if sys.argv[2] == "suma":
+        result = calc.plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = calc.minus(operando1, operando2)
+    elif sys.argv[2] == "multiplica":
+        result = calc.mult(operando1, operando2)
+    elif sys.argv[2] == "divide":
+            if operando2 == 0:
+                sys.exit("Division by zero is not allowed")
+            else:
+                result = calc.div(operando1, operando2)
+    print(result)
