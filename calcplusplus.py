e9f16879f39e7f658d8c62b8d1357094ec79e7a2
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-05 18:16:29 +0200

    Evitar repetir codigo

--- calcplusplus.py
+++ calcplusplus.py
@@ -2,34 +2,16 @@
 
 import sys
 import csv
-import calcoohija
+import calcplus
 
 calc = calcoohija.CalculadoraHija()
+opera = operacion.calcplus()
 resultado = 0
 
 with open(sys.argv[1]) as fichero:
     contenido = csv.reader(fichero)
 
     for row in contenido:
-        if row[0] == "suma":
-            for elemento in row[1:]:
-                resultado = calc.plus(int(resultado), int(elemento))
-            print(resultado)
-        if row[0] == "resta":
-            resultado = calc.minus(int(row[1]), int(row[2]))
-            for elemento in row[3:]:
-                resultado = calc.minus(int(resultado), int(elemento))
-            print(resultado)
-        if row[0] == "divide":
-            resultado = row[1]
-            for elemento in row[2:]:
-                if elemento == 0:
-                    sys.exit("Division by zero is not allowed")
-                else:
-                    resultado = calc.div(int(resultado), int(elemento))
-            print(resultado)
-        if row[0] == "multiplica":
-            resultado = calc.mult(int(row[1]), int(row[2]))
-            for elemento in row[3:]:
-                resultado = calc.mult(int(resultado), int(elemento))
-            print(resultado)
+     opera.operacion(row)
+
+    resultado = operacion(dato)
