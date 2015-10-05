2ae4c9748d603bd89b7b72675b4127e4091a54b7
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-05 10:42:19 +0200

    Pep8

--- calcoo.py
+++ calcoo.py
@@ -3,27 +3,28 @@
 
 import sys
 
+
 class Calculadora():
-	def plus(self, op1, op2):
-		return op1 + op2
+    def plus(self, op1, op2):
+        return op1 + op2
 
-	def minus(self, op1, op2):
-		return op1 - op2
-	
+    def minus(self, op1, op2):
+        return op1 - op2
+
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	calc = Calculadora()
+    calc = Calculadora()
 
-	if sys.argv[2] == "suma":
-		result = calc.plus(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = calc.minus(operando1, operando2)
-	else:
-		sys.exit('Operación sólo puede ser sumar o restar.')
+    if sys.argv[2] == "suma":
+        result = calc.plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = calc.minus(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
 
-	print(result)
+    print(result)
